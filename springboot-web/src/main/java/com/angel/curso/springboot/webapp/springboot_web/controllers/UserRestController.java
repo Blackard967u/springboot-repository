e13130680ae84a019d6573d9fe5a999049c61916
blackard967u <blackard967u@gmail.com>
2025-08-27 01:00:29 -0600

    user dto was created and implemented

--- springboot-web/src/main/java/com/angel/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ springboot-web/src/main/java/com/angel/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -9,16 +9,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.angel.curso.springboot.webapp.springboot_web.models.User;
+import com.angel.curso.springboot.webapp.springboot_web.models.dto.UserDto;
+
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@RequestMapping("/api")
 public class UserRestController {
 
-    @GetMapping("/details-user")    
-    public Map<String, Object> details(){
-        Map<String, Object> body = new HashMap<>();
-        body.put("title", "Hola mundo Springboot");
-        body.put("name", "Angel");
-        body.put("lastname", "Ocaña");
-        return body;
+    @GetMapping("/details-user")
+    public UserDto details() {
+        UserDto userDto = new UserDto();
+        User user = new User("Angel", "Ocaña");
+        userDto.setUser(user);
+        userDto.setTitle("Hola Mundo");
+
+        return userDto;
     }
+
+    /*
+     * @GetMapping("/details-user")
+     * public Map<String, Object> details() {
+     * User user = new User("Angel", "Ocaña");
+     * Map<String, Object> body = new HashMap<>();
+     * body.put("title", "Hola mundo Springboot");
+     * body.put("user", user);
+     * return body;
+     * }
+     */
 }
